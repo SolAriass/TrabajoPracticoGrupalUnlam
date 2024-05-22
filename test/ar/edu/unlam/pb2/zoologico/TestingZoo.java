aa6ef41c2d2ddc084b8a1d5ac7fd7c191e5a1597
SolAriass <ssolariaas@gmail.com>
2024-05-22 15:52:05 -0300

    Comienzo Tp

---
+++ test/ar/edu/unlam/pb2/zoologico/TestingZoo.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.zoologico;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestingZoo {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
