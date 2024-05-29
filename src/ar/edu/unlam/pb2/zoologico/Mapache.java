f3c3105524455144b88b0ced612896d2b5036769
SolAriass <ssolariaas@gmail.com>
2024-05-28 22:15:59 -0300

    agrego las busquedas por tipo de alimentacion

---
+++ src/ar/edu/unlam/pb2/zoologico/Mapache.java
@@ -0,0 +1,12 @@
+package ar.edu.unlam.pb2.zoologico;
+
+public class Mapache extends Animal {
+
+	public Mapache(Integer codigoDeReconocimiento, String nombreAnimal, Integer edad, Double peso,
+			TipoAlimentacion alimentacion, TipoSexo sexo, CategoriaAnimal clase) {
+		super(codigoDeReconocimiento, nombreAnimal, edad, peso, alimentacion, sexo, clase);
+	}
+
+	
+	
+}
