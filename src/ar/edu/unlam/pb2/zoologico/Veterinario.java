b7d8c5481a023174cf4a492162fe48079d3912ef
nicolassbon <agustindenis03@gmail.com>
2024-06-02 23:38:25 -0300

    Revert "probando merge"
    This reverts commit fefcb64cf38eb6a25768be42b3980f290c6cb0a7, reversing
    changes made to 9d5fd8bd3c6476a980191f8f0981926c25b8c405.

--- src/ar/edu/unlam/pb2/zoologico/Veterinario.java
+++ src/ar/edu/unlam/pb2/zoologico/Veterinario.java
@@ -1,41 +1,11 @@
 package ar.edu.unlam.pb2.zoologico;
 
-import ar.edu.unlam.pb2.zoologico.excepciones.NoSePudoAlimentarException;
-
-public class Veterinario extends Persona {
+public class Veterinario extends Personal {
 
 	public Veterinario(Integer identificacion, String nombreCompleto, Integer edad) {
 		super(identificacion, nombreCompleto, edad);
 	}
 
-	public Boolean alimentar(Animal animal, Comida comida, Integer cantidadComida) throws NoSePudoAlimentarException {
-		
-
-		if (cantidadComida > 0) {
-
-			if (animal.getTipoAlimentacion().equals(TipoAlimentacion.OMNIVORO)
-					&& (comida.equals(Comida.CARNE) || comida.equals(Comida.PLANTA))) {
-				animal.setPeso(animal.getPeso() * cantidadComida);
-				return  true;
-
-			} else if (animal.getTipoAlimentacion().equals(TipoAlimentacion.CARNIVORO) && comida.equals(Comida.CARNE)) {
-				animal.setPeso(animal.getPeso() * cantidadComida);
-				return true;
-
-			} else if (animal.getTipoAlimentacion().equals(TipoAlimentacion.HERBIVORO)
-					&& comida.equals(Comida.PLANTA)) {
-				animal.setPeso(animal.getPeso() * cantidadComida);
-				return true;
-
-			} else {
-				animal.setEstaEnfermo(true);
-				animal.setAlimentacionInapropiada(true);
-				return true;
-			}
-
-		}
-
-		throw new NoSePudoAlimentarException("algo fallo al intentar alimentar al animal");
-	}
+	
 
 }
--- src/ar/edu/unlam/pb2/zoologico/excepciones/NoSePudoAgregarPersonalInvalidoExcepcion.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unlam.pb2.zoologico.excepciones;
-
-public class NoSePudoAgregarPersonalInvalidoExcepcion extends Exception {
-
-	public NoSePudoAgregarPersonalInvalidoExcepcion(String string) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
